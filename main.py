b82bf5f4276dbcc89df631b648a57a9395ef1ee2
tushcmd <muturidavid854@gmail.com>
2024-12-21 12:53:29 +0300

    download weights & generate functions

--- main.py
+++ main.py
@@ -1,5 +1,56 @@
 import modal
 import io
 from fastapi import Response, HttpException, Query, Request
+from datetime import datetime, timezone
 import requests
 import os
+
+
+def download_model():
+    from diffusers import AutoPipelineForText2Image
+    import torch
+    
+    AutoPipelineForText2Image.from_pretrained(
+        "stabilityai/sdxl-turbo", 
+        torch_dtype=torch.float16,
+        variant="fp16"
+    )
+    
+image = (modal.Image.debian_slim().
+    pip_install("fastapi[standard]", "transformers", "accelerator", "diffusers", "requests")
+    .run_function(download_model))
+
+app = modal.App("stable-diffusion", image=image)
+
+
+@app.cls(
+    image=image,
+    gpu="A10G",
+    secrets=[modal.Secret.from_name("API_KEY")]
+)
+class Modal:
+    
+    @modal.build()
+    @modal.enter()
+    def load_weights(self):
+        from diffusers import AutoPipelineForText2Image
+        import torch
+        
+        self.pipe = AutoPipelineForText2Image(
+            "stabilityai/sdxl-turbo", 
+            torch_dtype=torch.float16,
+            variant="fp16"
+        )
+        
+        self.pipe.to("cuda")
+        self.API_KEY = os.environ["API_KEY"]
+        # self.pipe.enable_xformers_memory_efficient_attention()
+        
+    @modal.web_endpoint()
+    def generate(self, request: Request, prompt: str = Query(..., description="The prompt for image generation")):
+        image = self.pipe(prompt, num_inference_steps=1, guidance_scale=0.0).images[0]
+        
+        buffer = io.BytesIO()
+        image.save(buffer, format="JPEG")
+        
+        return Response(content=buffer.getvalue(), media_type="image/jpeg")
